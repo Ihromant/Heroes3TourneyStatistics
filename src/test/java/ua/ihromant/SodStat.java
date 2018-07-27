e500794a869be24bf2454e440d78d8d726e8ed50
Ihromant <vespertilion@gmail.com>
2018-07-27 23:07:14 +0300

    Fixed bug with random

--- src/test/java/ua/ihromant/SodStat.java
+++ src/test/java/ua/ihromant/SodStat.java
@@ -9,7 +9,6 @@ import org.testng.annotations.Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -50,9 +49,12 @@ public class SodStat {
         Document doc = Jsoup.parse(new URL(url), 10000);
         Elements records = doc.body().getElementById("collapseobj_tournament_reports")
                 .child(1).child(0).child(0).child(0).child(1).child(0).children();
-        List<GameResult> results = new ArrayList<GameResult>();
+        List<GameResult> results = new ArrayList<>();
         for (Element el : records) {
-            results.add(parse(el.child(0).text()));
+            GameResult r = parse(el.child(0).text());
+            if (r != null) {
+                results.add(parse(el.child(0).text()));
+            }
         }
         return results;
     }
@@ -67,14 +69,28 @@ public class SodStat {
         String[] firstPlayerInfo = text.substring(firstLeftBracket + 1, firstRightBracket).split(", ");
         String[] resultAndSecondPlayer = text.substring(firstRightBracket + 2, lastLeftBracket - 1).split(" ", 2);
         String[] secondPlayerInfo = text.substring(lastLeftBracket + 1, lastRightBracket).split(", "); // TODO add template info
-        PlayerInfo firstInfo = new PlayerInfo(dateAndFirstPlayer[1], Color.parse(firstPlayerInfo[0]),
-                Castle.parse(firstPlayerInfo[1]), Hero.parse(firstPlayerInfo[2]), Hero.parse(firstPlayerInfo[3])); // TODO add random checking
-        PlayerInfo secondInfo = new PlayerInfo(resultAndSecondPlayer[1], Color.parse(secondPlayerInfo[0]),
-                Castle.parse(secondPlayerInfo[1]), Hero.parse(secondPlayerInfo[2]), Hero.parse(secondPlayerInfo[3]));
+        PlayerInfo firstInfo = parsePlayerInfo(dateAndFirstPlayer[1], firstPlayerInfo); // TODO add random checking
+        PlayerInfo secondInfo = parsePlayerInfo(resultAndSecondPlayer[1], secondPlayerInfo);
+        if ("draw".equals(resultAndSecondPlayer[0])) {
+            return null; // TODO
+        }
         if ("def".equals(resultAndSecondPlayer[0])) {
             return new GameResult(firstInfo, secondInfo);
         } else {
             return new GameResult(secondInfo, firstInfo);
+        }
+    }
+
+    private static PlayerInfo parsePlayerInfo(String name, String[] split) {
+        if ("Random".equals(split[1])) {
+            return new PlayerInfo(name, Color.parse(split[0]),
+                    Castle.parse(split[2]), Hero.parse(split[3]), Hero.parse(split[4]));
+        } else if ("Random".equals(split[2])) {
+            return new PlayerInfo(name, Color.parse(split[0]),
+                    Castle.parse(split[1]), Hero.parse(split[3]), Hero.parse(split[4]));
+        } else {
+            return new PlayerInfo(name, Color.parse(split[0]),
+                    Castle.parse(split[1]), Hero.parse(split[2]), Hero.parse(split[3]));
         }
     }
 }
